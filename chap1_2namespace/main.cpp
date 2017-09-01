85fcb00fb1945553792499bae46cf04b12935629
Forexroo <forexroo@ForexroodeMacBook-Air.local>
2017-09-01 17:50:16 +0800

    Initial Commit

---
+++ chap1_2namespace/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chap1_2namespace
+//
+//  Created by Forexroo on 2017/9/1.
+//  Copyright © 2017年 bean. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
